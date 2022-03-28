c8bc7e63a93478184ec47e294370ad7058a09011
agoyy09 <prygsptr12@gmail.com>
2022-03-29 02:55:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exe1/ResultActivity.java
@@ -0,0 +1,37 @@
+package com.example.exe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.exe1.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView txTask, txJenis, txTime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_activity);
+
+        txTask = findViewById(R.id.tvTask);
+        txJenis = findViewById(R.id.tvJenistask);
+        txTime  = findViewById(R.id.tvTimetask);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String task = bundle.getString("a");
+        String jenis = bundle.getString("b");
+        String time = bundle.getString("c");
+
+        txTask.setText(task);
+        txJenis.setText(jenis);
+        txTime.setText(time);
+    }
+}
